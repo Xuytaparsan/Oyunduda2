cdb94c03d99d0c6f7bb9645c91d43147769c346c
Elnurnn <104248864+elnur628392@users.noreply.github.com>
2022-05-27 01:09:07 +0400

    update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5242999009:AAEQ2Xb5YNdEAZkgk1tskQWd3nnClqZChA8"
+TOKEN = "5339124677:AAElGbFEOY17uox8CVFYa54AEhbv30yWZO8"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
